180facb9257cd54f30a1878e1500bfd40d906ce6
samidare210 <72958390+samidare210@users.noreply.github.com>
2022-04-18 15:25:27 +0800

    new

---
+++ form.js
@@ -0,0 +1,20 @@
+function checkResult() {
+    var name = document.form.name.value;
+    var email = document.form.email.value;
+    var age = document.form.age.value;
+    var gender = document.form.gender.value;
+    var dateOfBirth = document.form.dob.value;
+    var telNumber = document.form.telNumber.value;
+
+    alert("Name: " + name
+        + "\nAge: " + age
+        + "\nEmail: " + email
+        + "\nGender: " + gender
+        + "\nDate of birth: " + dateOfBirth
+        + "\nTelephone Number: " + telNumber);
+
+    var result = confirm("Are you sure?");
+}
+function confirmSubmit() {
+    confirm("Are you sure?");
+}
